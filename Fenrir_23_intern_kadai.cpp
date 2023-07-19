b05a9c1484f1dbdcf859f57c0c29145a88ef072f
Ishikawa <user@DESKTOP-9FD3GU8>
2023-07-19 23:05:31 +0900

    リファクタリング。HashMapを採用した。

--- Fenrir_23_intern_kadai.cpp
+++ Fenrir_23_intern_kadai.cpp
@@ -1,54 +1,46 @@
 ﻿#include <iostream>
 #include <stack>
+#include <unordered_map>
 using namespace std;
 
 class Solution
 {
 public:
-	bool isValid(string bracketString)
-	{
-		//スタックを採用することにより左括弧を追跡して一致するかどうかを確認したい。
-		
-		stack<char> bracketStack;//スタックの初期化
+    bool isValid(const string& bracketString)
+    {
+        //ハッシュマップを採用しif文をスッキリさせた
 
-		for (char currentChar : bracketString)
-		{
-			//入力文字列を走査し左括弧の場合はプッシュ
-			if (currentChar == '(' || currentChar == '{' || currentChar == '[')
-			{
-				bracketStack.push(currentChar);
-			}
-			//右括弧の場合、スタックが空の場合は左括弧がないためfalse。
-			//それ以外の場合、スタックからポップし、現在の括弧に対応する括弧があるかを判定
-			//一致しない場合はfalseを返す
-			else
-			{
-				if (bracketStack.empty() ||
-					(currentChar == ')' && bracketStack.top() != '(') ||
-					(currentChar == '}' && bracketStack.top() != '{') ||
-					(currentChar == ']' && bracketStack.top() != '['))
-				{
-					return false;
-				}
-				bracketStack.pop();
-			}
-		}
-		//入力文字列を走査した後、スタックが空の場合はすべての括弧が一致していたことになるのでこれで完了となる。
-		return bracketStack.empty();
-	}
+        stack<char> bracketStack; // スタックの初期化
+        unordered_map<char, char> brackets = { {')', '('}, {'}', '{'}, {']', '['} }; // 対応する括弧を保存するマップ
+
+        // 入力文字列を走査
+        for (const char& currentChar : bracketString)
+        {
+            // 右括弧の場合、スタックが空か、スタックのトップが対応する括弧でない場合は、falseを返す
+            if (brackets.count(currentChar)) {
+                if (bracketStack.empty() || bracketStack.top() != brackets[currentChar]) {
+                    return false;
+                }
+                bracketStack.pop();
+            }
+            // 左括弧の場合、それをスタックにプッシュ
+            else {
+                bracketStack.push(currentChar);
+            }
+        }
+        // 入力文字列を走査した後、スタックが空なら、全ての括弧が一致していたと言える
+        return bracketStack.empty();
+    }
 };
 
 int main()
 {
-	Solution solution;
-	string input;
-	cout << "s=";
-	cin >> input;
-	bool result = solution.isValid(input);
-	if (result)
-		cout << "true" << endl;
-	else
-		cout << "false" << endl;
+    Solution solution;
+    string input;
+    cout << "s=";
+    cin >> input; // 入力
+    bool result = solution.isValid(input); // 括弧の組み合わせが有効かどうかを確認
+    cout << (result ? "true" : "false") << endl; // 結果を出力
 
-	return 0;
+    return 0;
 }
